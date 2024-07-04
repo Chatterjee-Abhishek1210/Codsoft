96616c9947c40ceeda92129fd6ff387b938156a2
Abhishek chatterjee <abhishekchatterjee510@gmail.com>
2024-07-04 14:40:51 +0530

    Completed

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,4 +1,4 @@
-// toggle icon navbar
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar= document.querySelector('.navbar');
 
@@ -10,16 +10,16 @@ menuIcon.onclick = () => {
 
 
 
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    const scrollY = window.scrollY;
+    let scrollY = window.scrollY;
 
     sections.forEach(section => {
-        const sectionTop = section.offsetTop - 150;
-        const sectionHeight = section.offsetHeight;
-        const sectionId = section.getAttribute('id');
+        let sectionTop = section.offsetTop - 150;
+        let sectionHeight = section.offsetHeight;
+        let sectionId = section.getAttribute('id');
 
         if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
             navLinks.forEach(link => link.classList.remove('active'));
